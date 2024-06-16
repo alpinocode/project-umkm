768172df6e3013f01fc01c4ccbb98421525a0d2d
alpinocode <alfinohasan629@gmail.com>
2024-06-16 21:47:09 +0700

    create a database configuration

---
+++ config/Database.js
@@ -0,0 +1,7 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize('projectumkm', 'root', '', {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+})
